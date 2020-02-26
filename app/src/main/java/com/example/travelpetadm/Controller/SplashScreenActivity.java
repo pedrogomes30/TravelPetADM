dafe0208d20cff2f6edd43dbdacecf5170d444de
pgs_c <pgs_cf@hotmail.com>
2020-02-25 23:27:54 -0300

    criação de temas, splash screen

---
+++ app/src/main/java/com/example/travelpetadm/Controller/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.travelpetadm.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.travelpetadm.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mostrarLogin();
+
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(SplashScreenActivity.this, Login.class);
+        startActivity(intent);
+        finish();
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+}
