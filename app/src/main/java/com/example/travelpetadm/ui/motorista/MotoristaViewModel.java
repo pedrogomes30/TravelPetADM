ebe70037bdbbd681a250c2d2c1daff85a87b31ad
pgs_c <pgs_cf@hotmail.com>
2020-03-24 23:38:27 -0300

    Atualização do main activity opara uso do navigation drawer ALTERAR GALARY

--- app/src/main/java/com/example/travelpetadm/ui/motorista/MotoristaViewModel.java
+++ app/src/main/java/com/example/travelpetadm/ui/motorista/MotoristaViewModel.java
@@ -1,14 +1,14 @@
-package com.example.travelpetadm.ui.slideshow;
+package com.example.travelpetadm.ui.motorista;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class MotoristaViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public MotoristaViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
